34f89e01437fb0da178efe89fa67643f81e6cccf
Facundo Masgrau <facundomasgrau@gmail.com>
2022-04-03 16:17:19 -0300

    change ip

--- src/config/api.tsx
+++ src/config/api.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 
 export const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "http://54.166.192.108/:3000",
 });
 
 api.interceptors.request.use(request => {
